9b4c94c2be571de512312137595982418f2ba4b1
Filip Pietrzak <62543350+phiphiii@users.noreply.github.com>
2026-01-19 19:21:27 +0100

    median.py added

---
+++ examples/median.py
@@ -0,0 +1,11 @@
+import cv2
+import torch
+import fastcv
+
+img = cv2.imread("../artifacts/test.jpg")
+img_tensor = torch.from_numpy(img).cuda()
+median_tensor = fastcv.median_blur(img_tensor, 10)
+median_np = median_tensor.squeeze(-1).cpu().numpy()
+cv2.imwrite("output_median.jpg", median_np)
+
+print("saved median blur image.")
